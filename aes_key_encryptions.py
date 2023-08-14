3af4ece0a9e227e077d340cece8e2cb7572e44bd
mohammad <127003546+mohammadreza1408@users.noreply.github.com>
2023-08-14 06:17:56 +0200

    Add files via upload

---
+++ aes_key_encryptions.py
@@ -0,0 +1,40 @@
+
+
+
+from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+import base64
+
+from Cryptodome.Hash import SHA256 
+
+
+def aes_key_encryptions(aes_key_byte):
+    key = RSA.import_key(open('tax_rsa.public').read())
+    cipher= PKCS1_OAEP.new(key,hashAlgo=SHA256.new()) 
+    signature =cipher.encrypt(aes_key_byte)
+    base64_bytes = base64.b64encode(signature)
+    base64_signature = base64_bytes.decode('ascii')
+    print()
+    return base64_signature
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
